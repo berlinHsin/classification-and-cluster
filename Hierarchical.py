c9ab56bd222f384c16f9f7a4f3886b9e90bb5236
BerlinHsin <berlin.hsin@gmail.com>
2015-01-03 04:05:54 +0800

    still work on Hierarchical

--- Hierarchical.py
+++ Hierarchical.py
@@ -10,29 +10,29 @@ class Hierarchical(lib) :
 		self.Distance = [] 
 		self.distanceType = self.cosineSim
 
-	def calDistance(self,nodeId) :
-		distanceLst = []
-		for curNode , data in enumerate(self.Datas) :
-			distance = 0
-			if nodeId < curNode :
-				distance , check = self.distanceType(data,self.Datas[nodeId])
-			elif curNode != nodeId :
-				distance = self.Distance[curNode][nodeId][1]
-			distanceLst.append( [curNode,distance] )
+	def calDistance(self) :
+		for nId , node in enumerate(self.Datas) :
+			distanceDict = {}
+			for nId2 , node2 in enumerate(self.Datas) :
+				if nId != nId2 :
+					distance , check = self.distanceType(nId,nId2)
+					distanceDict.update({nId2:distance})
+			self.Distance.update({nId,distanceDict})
 
-		distance , check = self.distanceType([1],[2])
-		reverseFlag = check(10,0)
-		return sorted(distanceLst,key=lambda x :x[1],reverse=reverseFlag)
-
-	def merge(self):
-		distance = []
-		for groupId , group in enumerate(self.Group)
-			pass
+	def calGroupDist(self) :
+		self.Distance = []
+		for gId , group in enumerate(self.Group) :
+			self.calDistance(group)
 	
+	def merge(self) :
+		self.calGroupDist()
+
 	def main(self) :
 		for nodeId in range(len(self.Datas)) :
-			self.Distance.append(self.calDistance(nodeId))
 			self.Group.update({nodeId:[nodeId]})
+		self.calDistance()
+
+
 
 		while(len(self.Group)>self.K) :
 			self.merge()
